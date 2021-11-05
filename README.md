339bf42ae5785f7668e60f1c958f878327d17491
Anderson Leonardo <and.seijuro21@gmail.com>
2021-11-05 01:40:52 -0300

    Terceiro Commit
    Terceiro Commit 05/11/2021 01:40h

--- README.md
+++ README.md
@@ -10,5 +10,15 @@ Subindo HTML e CSS
 Segundo Commit:
 Adicionando o arquivo .Js junto com alguns eventos para abrir e fechar o (Modal Cadastrar Cliente)
 
+05/11/2021 01:40h Terceiro Commit:
+Criação dos metodos: Create, Read, Update e Delete (todos estão funcionando via LocalStorage) Para testar, basta abrir o console e enviar os comandos:
+
+createCliene(tempClient) Para criar um novo cliente, lembrando que isso ainda está setado em uma variavel global
+
+readClient() para trazer os dados enviado pelo tempClient
+
+updateClient funciona por posições, é necessário setar então a posição que os dados devem ser alterados (lembrando que os novos dados devem ser setados no arquivo JS e atraves do Index, o JSON será alterado)
+
+deleteClient também está funcionnado por posições, para acionar este basta pedir o seguinte comando deleteClient(1) ou mais valores.
 --------------------------------------------------------------------------------------------------------------------------
 
--- main.js
+++ main.js
@@ -6,6 +6,44 @@ const openModal = () => document.getElementById('modal')
 const closeModal = () => document.getElementById('modal')
     .classList.remove('active')
 
+
+const tempClient = {
+    nome: "Anderson2",
+    email: "anderson@anderson",
+    celular: "11222223333",
+    cidade: "são paulo"
+}
+
+const getLocalStorage = () => JSON.parse(localStorage.getItem("db_client")) ?? [] //Pega oq tem no Banco de dados (LocalStorage) e transforma em JSON e armazena em uma variavel db_client --- No final inseri a validação ??, o codigo não for valido, eu retorno um array vazio
+const setLocalStorage = (dbClient) => localStorage.setItem("db_client", JSON.stringify(dbClient)) //seta os itens, e JSON vai transformar o meu db_client string ao eniar ao LocalStorage ---  //Criando um Local Storage, lembrando que temos a Key > Value
+
+//Delete
+const deleteClient = (index) => {
+    const dbClient = readClient()
+    dbClient.splice(index,1)
+    setLocalStorage(dbClient)
+}
+
+//Update
+const updateCliente = (index, client) => {
+    const dbClient = readClient()
+    dbClient[index] = client
+    setLocalStorage(dbClient)
+}
+
+//Read
+const readClient = () => getLocalStorage()
+
+//Create
+const createClient = (client) => {
+    const dbClient = getLocalStorage()
+    dbClient.push (client) //o push irá empurrar mais um (cliente) dentro do array
+    setLocalStorage(dbClient)
+
+}
+
+
+
 //Botão cadastrar cliente espera o evento de click para executar o openModal
 document.getElementById('cadastrarCliente')
     .addEventListener('click', openModal)
