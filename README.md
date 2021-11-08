12b6b885e22337f49adbf6a48f7c13c0252e0aca
Anderson <and.seijuro21@gmail.com>
2021-11-08 00:48:18 -0300

    Quarto Branch
    Quarto Branch, botão de salvar clientes
    07/11/2021 20:41hrs

--- README.md
+++ README.md
@@ -6,9 +6,12 @@ Descritivo dos Commits:
 
 Primeiro Commit:
 Subindo HTML e CSS
+--------------------------------------------------------------------------------------------------------------------------
 
 Segundo Commit:
 Adicionando o arquivo .Js junto com alguns eventos para abrir e fechar o (Modal Cadastrar Cliente)
+
+--------------------------------------------------------------------------------------------------------------------------
 
 05/11/2021 01:40h Terceiro Commit:
 Criação dos metodos: Create, Read, Update e Delete (todos estão funcionando via LocalStorage) Para testar, basta abrir o console e enviar os comandos:
@@ -19,6 +22,10 @@ readClient() para trazer os dados enviado pelo tempClient
 
 updateClient funciona por posições, é necessário setar então a posição que os dados devem ser alterados (lembrando que os novos dados devem ser setados no arquivo JS e atraves do Index, o JSON será alterado)
 
-deleteClient também está funcionnado por posições, para acionar este basta pedir o seguinte comando deleteClient(1) ou mais valores.
+deleteClient também está funcionado por posições, para acionar este basta pedir o seguinte comando deleteClient(1) ou mais valores.
+
+--------------------------------------------------------------------------------------------------------------------------
+07/11/2021 20:41h Quarto Commit:
+Funcionalidade do botão Salvar dentro do modal Cadastrar Cliente, funcionando. (Chamando o UpdateTable para inserir as novas linhas contendo as informações de usuário já cadastrados no LocalStorage)
 --------------------------------------------------------------------------------------------------------------------------
 
--- main.js
+++ main.js
@@ -2,16 +2,10 @@
 const openModal = () => document.getElementById('modal')
     .classList.add('active')
 
-//Retira a classe active quando o closeModal for chamado
-const closeModal = () => document.getElementById('modal')
-    .classList.remove('active')
-
-
-const tempClient = {
-    nome: "Anderson2",
-    email: "anderson@anderson",
-    celular: "11222223333",
-    cidade: "são paulo"
+//Limpa os campos do Modal e depois fecha o modal
+const closeModal = () => {
+    clearFields() //limpa os campos
+    document.getElementById('modal').classList.remove('active')
 }
 
 const getLocalStorage = () => JSON.parse(localStorage.getItem("db_client")) ?? [] //Pega oq tem no Banco de dados (LocalStorage) e transforma em JSON e armazena em uma variavel db_client --- No final inseri a validação ??, o codigo não for valido, eu retorno um array vazio
@@ -39,10 +33,52 @@ const createClient = (client) => {
     const dbClient = getLocalStorage()
     dbClient.push (client) //o push irá empurrar mais um (cliente) dentro do array
     setLocalStorage(dbClient)
-
 }
 
+const isValidFields = () => {
+   return document.getElementById("form").reportValidity() //Irá verificar se todas as regras foram seguidas
+}
 
+const clearFields = () => {
+    const fields = document.querySelectorAll('.modal-field')
+    fields.forEach(field => field.value = "") // vai igualar os campos igual a vazio
+}
+
+const saveClient =() => {//se a regra anterior do reportValidity for comprida, o JSON abaixo será criado a partir dos dados inseridos no form
+    if (isValidFields()){
+        const client = {
+            nome: document.getElementById('nome').value,
+            email: document.getElementById('email').value,
+            celular: document.getElementById('celular').value,
+            cidade: document.getElementById('cidade').value,
+        }
+        createClient(client) //cria novo cliente e envia para o LocalStorage
+        
+        closeModal() 
+    }
+}
+
+const createRow = (client) => {
+    const newRow = document.createElement('tr')
+    newRow.innerHTML = `
+        <td>${client.nome}</td>
+        <td>${client.email}</td>
+        <td>${client.celular}</td>
+        <td>${client.cidade}</td>
+        <td>
+            <button type="button" class="button green">editar</button>
+            <button type="button" class="button red">excluir</button>
+        </td>
+    `
+    document.querySelector("#tableClient>tbody").appendChild(newRow)//inserindo as informações acima preenchidas no tbody
+}
+
+const updateTable = () => {//Quando updateTable for acionado
+    const dbClient = readClient() //vai ler o que tem no LocalStorage
+    dbClient.forEach(createRow) //vai trazer o array com os JSON e vai enviar para o createRow
+
+}
+updateTable()
 
 //Botão cadastrar cliente espera o evento de click para executar o openModal
 document.getElementById('cadastrarCliente')
@@ -51,3 +87,7 @@ document.getElementById('cadastrarCliente')
 //modalClose é o meu botão "X" para fechar a janela, aqui o botão irá esperar o click para executar a função do closeModal
 document.getElementById('modalClose')
     .addEventListener('click', closeModal)
+
+//interação com o botão salvar do modal 
+document.getElementById("salvar")
+    .addEventListener('click', saveClient)
